7a751faf9766190dc9d002743a5d5f101cc43039
jacksonporter <jackson_porter@outlook.com>
2018-07-06 15:10:39 -0600

    Fixing SQL problem

--- EMServerApp/src/emserverapp/ClientCommandProcessor.java
+++ EMServerApp/src/emserverapp/ClientCommandProcessor.java
@@ -113,7 +113,7 @@ public class ClientCommandProcessor implements Runnable{
 									+ " emdb.'User'.lastname, emdb.'User'.email, emdb.'User'.phonenumber, emdb.'User'.isAdmin,"
 									+ " emdb.'User'.isManager, emdb.'User'.isEmployee, emdb.'User'.isOutsideManager,"
 									+ " emdb.'User'.isGroupContact,	emdb.'User'.isOutsideVendor FROM emdb.'User'"
-									+ " WHERE username = '?' AND active = 1;");
+									+ " WHERE username=? AND active = 1;");
 							preparedStatement.setString(1, subCommandSplit[1]);
 							System.err.println("ABOUT TO QUERY: " + preparedStatement.toString());
 							resultSet = preparedStatement.executeQuery();
